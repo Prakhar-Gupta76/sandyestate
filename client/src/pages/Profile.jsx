b570eeca839b308fbd1df5332af8e4430ecfccce
Prakhar-Gupta <pg0002003@gmail.com>
2024-05-02 18:18:53 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase.js'
-import { updateUserFailure, updateUserSuccess, updateUserStart } from '../redux/user/userSlice.js'
+import { updateUserFailure, updateUserSuccess, updateUserStart, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js'
 
 export default function Profile() {
     const fileref = useRef(null)
@@ -66,6 +66,22 @@ export default function Profile() {
             dispatch(updateUserFailure(error.message))
         }
     }
+    const handleDeleteUser = async () => {
+        try {
+            dispatch(deleteUserStart())
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                dispatch(deleteUserFailure(data.message))
+                return;
+            }
+            dispatch(deleteUserSuccess(data));
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message))
+        }
+    }
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
@@ -93,7 +109,7 @@ export default function Profile() {
                 <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
             </form>
             <div className='flex justify-between mt-5'>
-                <span className='text-red-700 cursor-pointer'>Delete Account</span>
+                <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
                 <span className='text-red-700 cursor-pointer'>Sign Out</span>
             </div>
             <p className='text-red-700 mt-5'>{error ? error : ''}</p>
