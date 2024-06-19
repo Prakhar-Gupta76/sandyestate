4e9c34a6c0bd0c22087797367e236f7d3e8d3947
Prakhar-Gupta <pg0002003@gmail.com>
2024-06-19 15:46:04 +0530

    create search page UI

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -62,7 +62,6 @@ export const getUserListings = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
     try {
-
         const user = await User.findById(req.params.id);
         if (!user) {
             return next(errorHandler(404, 'User not found!'))
--- api/index.js
+++ api/index.js
@@ -6,18 +6,21 @@ import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
 dotenv.config()
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Database connected');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to DB')
-})
-    .catch((err) => {
-        console.log(err)
-    })
 const app = express()
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
-
+connectDB()
 app.use(express.json())
 
 app.use(cookieParser());
